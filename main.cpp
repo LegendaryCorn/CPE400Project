9976d87931bfc228144144331f998f8c36ed3e4f
LegendaryCorn <43460114+LegendaryCorn@users.noreply.github.com>
2021-03-20 14:03:36 -0700

    Nodes and Links Setup
    Added code that will set up nodes and links.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,96 @@
 // Crystal Atoz, Korben DiArcangel, Joshua Insorio
 
 #include <iostream>
+#include <string>
+#include <map>
 #include "node.h"
 
 using namespace std;
 
+void createLink(map<string,node*> nodeList, string nodeA, string nodeB, int weight);
+
 int main()
 {
+	map<string,node*> nodeList;
+	
+	
+	//----------------------------
+	// Initialize Nodes
+	//----------------------------
+    nodeList["California"] = new node("California");
+    nodeList["Nevada"] = new node("Nevada");
+    nodeList["Colorado"] = new node("Colorado");
+    nodeList["Arizona"] = new node("Arizona");
+    nodeList["New Mexico"] = new node("New Mexico");
+    nodeList["Utah"] = new node("Utah");
+    nodeList["Idaho"] = new node("Idaho");
+    nodeList["Oregon"] = new node("Oregon");
+    
+    //----------------------------
+	// Initialize Links
+	//----------------------------
+    createLink(nodeList, "California","Nevada",5);
+    createLink(nodeList, "Utah","Nevada",10);
+    createLink(nodeList, "New Mexico","Idaho",4);
+    createLink(nodeList, "Arizona","Colorado",7);
+    createLink(nodeList, "Oregon","California",6);
+    createLink(nodeList, "Nevada","Idaho",2);
+    createLink(nodeList, "Arizona","New Mexico",8);
+    createLink(nodeList, "Oregon","Utah",12);
+    createLink(nodeList, "Colorado","California",2);
+    createLink(nodeList, "Nevada","New Mexico",6);
+	
+	//----------------------------
+	// Testing the Nodes (Should be removed later)
+	//----------------------------
+	
+	for(map<string,node*>::iterator a = nodeList.begin(); a != nodeList.end(); a++){
+		for(map<string,node*>::iterator b = nodeList.begin(); b != nodeList.end(); b++){
+			if(a->second->getLinkStatus(b->first) >= 0)
+			{
+				cout << a->first << " - " << b->first << " " << a->second->getLinkStatus(b->first) << endl;
+			}
+		}
+	}
+	
+	//----------------------------
+	// Deleting the Nodes
+	//----------------------------
+	for(map<string,node*>::iterator i = nodeList.begin(); i != nodeList.end(); i++){
+		delete i->second;
+	}
+	
     return 0;
+}
+
+//-----------------------------------------------------------------------------
+//
+// void createLink
+//
+// This function should create a link between A and B.
+//
+// inputs:
+//    nodeList: The map containing the pointers to the nodes.
+//    nodeA: The name of the first node.
+//    nodeB: The name of the second node.
+//    weight: the weight of the link.
+//
+//-----------------------------------------------------------------------------
+
+void createLink(map<string,node*> nodeList, string nodeA, string nodeB, int weight)
+{
+	//----------------------------
+	// Checking if the link is possible
+	//----------------------------
+	if((nodeList.find(nodeA) == nodeList.end()) || (nodeList.find(nodeB) == nodeList.end()))
+	{
+		cout << "Error setting connection " << nodeA << " - " << nodeB << ": One or more of the nodes does not exist." << endl;
+		return;
+	}
+	
+	//----------------------------
+	// Forming the link
+	//----------------------------
+	nodeList[nodeA]->setLink(nodeB,nodeList[nodeB],weight);
+	nodeList[nodeB]->setLink(nodeA,nodeList[nodeA],weight);
 }
--- node.h
+++ node.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -11,9 +12,12 @@ class node
 {
     public:
         node();
+        node(string nname);
         node(int ndata, string nname);
 
-        void setLink();
+        void setLink(string nname, node* npointer, int nweight);
+        
+        int getLinkStatus(string nname);
         bool getStatus();
         int getWeight();
         string getName();
@@ -22,15 +26,28 @@ class node
         int data;
         bool status;
         string name;
-        vector<string> neighbors;
-        vector<int> neighborsWeights;
+        map<string,node*> neighbors;
+        map<string,int> neighborsWeights;
 };
+
+//-----------------------------------------------------------------------------
+//
+// CONSTRUCTORS
+//
+//-----------------------------------------------------------------------------
 
 node::node()
 {
     data = '\n';
     name = "";
     status = true;
+}
+
+node::node(string nname)
+{
+	data = '\n';
+	name = nname;
+	status = true;
 }
 
 node::node(int ndata, string nname)
@@ -40,10 +57,81 @@ node::node(int ndata, string nname)
     status = true;
 }
 
+//-----------------------------------------------------------------------------
+//
+// void setLink
+//
+// This function establishes a link to another node.
+//
+// inputs:
+//    nname: The name of the node to connect to.
+//    npointer: The pointer to the node.
+//    nweight: The weight of the link.
+//
+//-----------------------------------------------------------------------------
+
+void node::setLink(string nname, node* npointer, int nweight)
+{
+	neighbors[nname] = npointer;
+	neighborsWeights[nname] = nweight;
+}
+
+//-----------------------------------------------------------------------------
+//
+// int getLinkStatus
+//
+// Gets the status of the link to a node.
+//
+// inputs:
+//    nname: The name of the node to check the connection with.
+//
+// outputs:
+//    int: A number that determines the status of the link.
+//       0 or above: The link exists and has this weight.
+//       -1: The link doesn't exist.
+//       -2: The link exists, but the node is currently down.
+//
+//-----------------------------------------------------------------------------
+int node::getLinkStatus(string nname)
+{
+	if(neighbors.find(nname) == neighbors.end())
+	{
+		return -1;
+	}
+	if(!neighbors[nname]->getStatus())
+	{
+		return -2;
+	}
+	
+	return neighborsWeights[nname];
+}
+
+//-----------------------------------------------------------------------------
+//
+// bool getStatus
+//
+// Checks the node's status.
+//
+// outputs:
+//    status: The current status of the node.
+//
+//-----------------------------------------------------------------------------
+
 bool node::getStatus()
 {
     return status;
 }
+
+//-----------------------------------------------------------------------------
+//
+// string getName
+//
+// This function get's the node's name.
+//
+// outputs:
+//    name: The name of the node.
+//
+//-----------------------------------------------------------------------------
 
 string node::getName()
 {
